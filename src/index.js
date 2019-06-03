18ce8e14b9d8a88cbe3a1a599e5dc039cad4a771
Eric Waring <ericw@the-feel.co.uk>
2019-06-03 13:22:34 +0100

    Add redux to auth

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import { Provider } from "react-redux";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 //import { createStore, applyMiddleware } from "redux";
 //import thunk from "redux-thunk";
 
 import App from "./components/App";
-//import reducers from "./reducers";
+import reducers from "./reducers";
 
+const store = createStore(reducers);
 //const store = createStore(reducers, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.querySelector("#root"));
-
-//ReactDOM.render(
-//  <Provider store={store}>
-//    <App />
-//  </Provider>,
-//  document.querySelector("#root")
-//);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
